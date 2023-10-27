cee3f6c1c2de402c52e8db3115126074451f07dd
NAM\ACER <namldph24284@fpt.edu.vn>
2023-10-27 20:53:46 +0700

    me thang Tuan noi nhieu

--- src/main/java/com/example/sneaker_sophia/controller/websiteshop/ProductWebController.java
+++ src/main/java/com/example/sneaker_sophia/controller/websiteshop/ProductWebController.java
@@ -1,6 +1,11 @@
 package com.example.sneaker_sophia.controller.websiteshop;
 
+import com.example.sneaker_sophia.entity.Hang;
+import com.example.sneaker_sophia.repository.ChiTietGiayRepository;
+import com.example.sneaker_sophia.repository.HangRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("sophia-store")
 public class ProductWebController {
 
+    @Autowired
+    private HangRepository hangRepository;
+    @Autowired
+    private ChiTietGiayRepository chiTietGiayRepository;
+
     @GetMapping("/product")
-    public String home(){
-        return "website/product-details-default";
+    public String home(Model model){
+//        model.addAttribute("danhSachHang", this.hangRepository.findAll());
+        model.addAttribute("danhSachroduct", this.chiTietGiayRepository.findAll());
+        return "website/productwebsite/shop-grid-sidebar-left";
     }
 }
