ac15b0cbdafcbe05e84a6ea863c7f045641e37eb
doTuan <Tuan191203@gmail.com>
2023-10-31 17:46:23 +0700

    giỏ hàng

---
+++ src/main/java/com/example/sneaker_sophia/repository/GioHangChiTietRepository.java
@@ -0,0 +1,9 @@
+//package com.example.sneaker_sophia.repository;
+//
+//import com.example.sneaker_sophia.entity.GioHangChiTiet;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.UUID;
+//
+//public interface GioHangChiTietRepository extends JpaRepository<GioHangChiTiet, UUID> {
+//}
