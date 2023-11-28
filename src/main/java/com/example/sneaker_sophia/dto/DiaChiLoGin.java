a74bd817806ce4cac89986171dd505ac9660b941
NAM\ACER <namldph24284@fpt.edu.vn>
2023-11-29 00:36:25 +0700

    validate diachi and update giam gia and vnpay

--- src/main/java/com/example/sneaker_sophia/dto/DiaChiLoGin.java
+++ src/main/java/com/example/sneaker_sophia/dto/DiaChiLoGin.java
@@ -1,12 +1,10 @@
 package com.example.sneaker_sophia.dto;
 
-
 import com.example.sneaker_sophia.entity.DiaChi;
 import com.example.sneaker_sophia.entity.TaiKhoan;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -14,12 +12,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class DiaChiDTO {
+public class DiaChiLoGin {
     private String id;
     @NotBlank(message = "Vui lòng nhập tên người nhận")
     private String ten;
     @NotBlank(message = "Vui lòng nhập số điện thoại người nhận")
-    @Pattern(regexp = "0\\d{9}", message = "Số điện thoại không hợp lệ")
+
     private String sdt;
     @NotBlank(message = "Vui lòng nhập Địa chỉ cụ thể")
     private String diaChiCuThe;
@@ -30,10 +28,6 @@ public class DiaChiDTO {
     @NotNull(message = "Vui lòng chọn tỉnh")
     private Integer tinh;
 
-    @NotBlank(message = "Vui lòng nhập địa chỉ email")
-    @Email(message = "Địa chỉ email không hợp lệ")
-    private String email;
-
     private Integer diaChiMacDinh;
 
     private Integer trangThai;
@@ -41,7 +35,6 @@ public class DiaChiDTO {
     private TaiKhoan taiKhoan;
 
     public DiaChi loadDiaChiDTO(DiaChi diaChi) {
-        diaChi.setId(getId());
         diaChi.setTen(getTen());
         diaChi.setSdt(getSdt());
         diaChi.setDiaChiCuThe(getDiaChiCuThe());
@@ -50,7 +43,6 @@ public class DiaChiDTO {
         diaChi.setTinh(getTinh());
         if (getTaiKhoan() != null) {
             TaiKhoan taiKhoan = getTaiKhoan();
-            taiKhoan.setEmail(getEmail());
             diaChi.setTaiKhoan(taiKhoan);
         }
 
@@ -59,5 +51,4 @@ public class DiaChiDTO {
 
         return diaChi;
     }
-
 }
--- src/main/java/com/example/sneaker_sophia/service/ThanhToanService.java
+++ src/main/java/com/example/sneaker_sophia/service/ThanhToanService.java
@@ -1,5 +1,7 @@
 package com.example.sneaker_sophia.service;
 
+import com.example.sneaker_sophia.dto.DiaChiDTO;
+import com.example.sneaker_sophia.dto.DiaChiLoGin;
 import com.example.sneaker_sophia.entity.*;
 import com.example.sneaker_sophia.repository.*;
 import jakarta.annotation.Resource;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ThanhToanService {
@@ -44,7 +47,7 @@ public class ThanhToanService {
     @Resource(name = "hoaDonRepository")
     HoaDonRepository hoaDonRepository;
 
-    public void thucHienThanhToan(String email, List<GioHangChiTiet> cartItems, Integer hinhThucThanhToan) {
+    public void thucHienThanhToan(String email, List<GioHangChiTiet> cartItems, Integer hinhThucThanhToan,String diaChi, String tinh, String huyen, String xa) {
         TaiKhoan taiKhoan = this.loginRepository.findByEmail(email);
         double total = 0.0;
         for (GioHangChiTiet cartItem : cartItems) {
@@ -63,11 +66,12 @@ public class ThanhToanService {
         hoaDon.setLoaiHoaDon(3);
         hoaDon.setTenKhachHang(taiKhoan.getTen());
         hoaDon.setSoDienThoai(taiKhoan.getSdt());
-        hoaDon.setDiaChi(diaChiTamChu.taoDiaChiString(taiKhoan.getDiaChiList()));
+        hoaDon.setDiaChi(diaChi + "-" + xa + "-" + huyen + "-" + tinh);
         hoaDon.setPhiShip(20000.0);
         hoaDon.setTrangThai(3);
         hoaDon.setTongTien(0.0);
         hoaDon.setTienThua(0.0);
+        hoaDon.setTongTien(tongTienDonHang);
         hoaDon.setKhuyenMai(tongTienGiam);
         HoaDon savedHoaDon = hoaDonWebRepository.save(hoaDon);
         for (GioHangChiTiet cartItem : cartItems) {
@@ -77,46 +81,58 @@ public class ThanhToanService {
             int soLuongMua = cartItem.getSoLuong();
             int soLuongHienTai = chiTietGiay.getSoLuong();
 
+            int soLuongPhieuGiamDaSuDung = 0;
             if (soLuongHienTai >= soLuongMua) {
                 List<CTG_KhuyenMai> listCTG_KM = chiTietGiay.getListCTG_KM();
-                int soLuongPhieuGiamDaSuDung = 0;
-
+                // Tính toán số lượng giảm và giảm giá tương ứng
                 for (CTG_KhuyenMai ctg : listCTG_KM) {
+                    // Kiểm tra trạng thái giảm giá
                     if (ctg.getId().getVoucher().getTrangThai() == 1 && ctg.getId().getVoucher().getSoLuong() > 0) {
-                        int soLuongGiamApDung = Math.min(ctg.getId().getVoucher().getSoLuong() - soLuongPhieuGiamDaSuDung, soLuongMua);
+                        if (ctg.getId().getVoucher() != null) {
+                            soLuongGiam = ctg.getId().getVoucher().getSoLuong();
+                        }
+                        int soLuongGiamApDung = Math.min(soLuongGiam - soLuongPhieuGiamDaSuDung, soLuongMua);
 
-                        double donGia = chiTietGiay.getGia();
+                        // Cập nhật số lượng giảm giá của sản phẩm
+                        tongSoLuongGiam = soLuongGiamApDung;
+                        // Cập nhật giảm giá của sản phẩm
                         phanTramGiam = ctg.getId().getVoucher().getPhanTramGiam();
-                        double tienGiam = donGia * phanTramGiam / 100 * soLuongGiamApDung;
-
-                        tongSoLuongGiam += soLuongGiamApDung;
-                        tongGiamGia += phanTramGiam;
+                        int giamGia = phanTramGiam * soLuongGiam;
+                        tongGiamGia = giamGia;
+                        // Cập nhật tổng số tiền giảm
+                        double donGia = chiTietGiay.getGia();
+                        int giam = ctg.getId().getVoucher().getPhanTramGiam();
+                        double tienGiam = donGia * giam / 100 * soLuongGiamApDung;
                         tongTienGiam += tienGiam;
+                        tongTienDonHang = total - tongTienGiam;
                         soLuongPhieuGiamDaSuDung += soLuongGiamApDung;
 
+                        // Trừ đi số lượng đã sử dụng từ bảng giảm giá
                         ctg.getId().getVoucher().setSoLuong(ctg.getId().getVoucher().getSoLuong() - soLuongGiamApDung);
                         voucherRepository.save(ctg.getId().getVoucher());
                     }
                 }
 
+                // Cập nhật số lượng tồn sau khi giảm giá
                 chiTietGiay.setSoLuong(soLuongHienTai - soLuongMua);
                 chiTietGiayRepository.save(chiTietGiay);
             } else {
                 return; // Xử lý khi số lượng không đủ
             }
 
-            hoaDonChiTiet.setHoaDon(savedHoaDon);
             hoaDonChiTiet.setChiTietGiay(cartItem.getId().getChiTietGiay());
             hoaDonChiTiet.setSoLuong(cartItem.getSoLuong());
             hoaDonChiTiet.setDonGia(cartItem.getId().getChiTietGiay().getGia());
-            hoaDonChiTiet.setPhanTramGiam(tongGiamGia);
+            hoaDonChiTiet.setPhanTramGiam(phanTramGiam);
             hoaDonChiTiet.setSoLuongGiam(tongSoLuongGiam);
+            hoaDonChiTiet.setHoaDon(savedHoaDon);
             this.hoaDonChiTietRepository.save(hoaDonChiTiet);
         }
 
         savedHoaDon.setTongTien(total - tongTienGiam);
         savedHoaDon.setKhuyenMai(tongTienGiam);
         hoaDonWebRepository.save(savedHoaDon);
+        session.setAttribute("maHD", savedHoaDon.getMaHoaDOn());
 
 
         HinhThucThanhToan hinhThuc = new HinhThucThanhToan();
@@ -136,6 +152,17 @@ public class ThanhToanService {
         this.emailService.guiEmailXacNhanThanhToan(email, savedHoaDon);
     }
 
+
+    public void capNhatDiaChi(DiaChiLoGin diaChiDTO, TaiKhoan taiKhoan) {
+        Optional<DiaChi> defaultDiaChi = taiKhoan.getDiaChiList().stream()
+                .filter(diaChi -> diaChi.getDiaChiMacDinh() == 1)
+                .findFirst();
+
+        defaultDiaChi.ifPresent(existingDefaultDiaChi -> {
+            diaChiDTO.loadDiaChiDTO(existingDefaultDiaChi); // Cập nhật thông tin địa chỉ
+            this.diaChiTamChu.save(existingDefaultDiaChi);
+        });
+    }
 
     public List<GioHangChiTiet> getCartItemsByEmail(String email) {
         TaiKhoan taiKhoan = this.loginRepository.findByEmail(email);
